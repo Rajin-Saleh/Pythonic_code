6a7f429eb9f21788fb1023ae0cf55fc002dbb1a2
Prince <75819360+Rajin-Saleh@users.noreply.github.com>
2024-03-18 23:23:00 +0600

    userData.py

--- userData.py
+++ userData.py
@@ -1,28 +1,28 @@
-# collecting info from a user
+# Collecting info from a user
 
- fist_name = input("What is you name? ").title()
- country = input("WHat is your Country? ").title()
- city = input("WHat city do you live in? ").title()
+fist_name = input("What is you name? ").title()
+country = input("WHat is your Country? ").title()
+city = input("WHat city do you live in? ").title()
 
- print(f"{fist_name} lives in {city}, {country}.")
+print(f"{fist_name} lives in {city}, {country}.")
 
-#create 2 variable to gather user info
+#create 2 variables to gather user info
 
- rent_price = int(input("Rental car price per day: ")) 
- rent_days = int(input("How long do you intent to hire it? ")) 
- total_cost = ( rent_days * rent_price)
+rent_price = int(input("Rental car price per day: ")) 
+rent_days = int(input("How long do you intent to hire it? ")) 
+total_cost = ( rent_days * rent_price)
 
  print(f"Total car price is : {total_cost}")
 
-# 3 variable for shopping packages
+# 3 Variable for shopping packages
 
- bag1 = int(input("Package weight 1? =  "))
- bag2 = int(input("Package weight 2? =  "))
- bag3 = int(input("Package weight 3? =  "))
+bag1 = int(input("Package weight 1? =  "))
+bag2 = int(input("Package weight 2? =  "))
+bag3 = int(input("Package weight 3? =  "))
 
- total_weight = (bag1+bag2+bag3) * 0.8
+total_weight = (bag1+bag2+bag3) * 0.8
 
- print(f"Shipment total: {total_weight}")
+print(f"Shipment total: {total_weight}")
 
 # create user Id
 name = input("What is you name? ").title()
