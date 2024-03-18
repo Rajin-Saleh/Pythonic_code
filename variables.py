15fac737b6ca3926911d95afc2f17731116ce291
Prince <75819360+Rajin-Saleh@users.noreply.github.com>
2024-03-18 23:21:58 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,34 @@
+# variables
+
+# There re 5 people travelling
+
+flight_cost  = 455
+insurance = 19.99
+passport = 110
+baggage = 45
+
+total = ( flight_cost + insurance + passport + baggage ) * 5
+
+print(total) 
+
+
+# 1 variable for adult and 1 for children
+
+adult = 2
+children = 2
+
+adult_price = 55
+children_price = 27
+
+total = (adult * adult_price) + (children * children_price)
+print(f"Six flag total: {total}")
+
+normal_price = 500
+family_stay_days = 7
+total = ((normal_price * family_stay_days) * 0.7)
+
+print(f"Special price: {total}")
+
+
+
+
